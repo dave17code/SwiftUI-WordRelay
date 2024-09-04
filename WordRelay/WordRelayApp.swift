f988aeacbc61445542525ad53ba3ef5dc4213ca9
dave17code <dave17code@gmail.com>
2024-09-04 21:14:13 +0900

    Initial Commit

---
+++ WordRelay/WordRelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordRelayApp.swift
+//  WordRelay
+//
+//  Created by 김두운 on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WordRelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
